dfd68793acfecd411fb261e65e55f12b40b64189
Angel Antonio Barco Alfaro <angel.barco@kavak.com>
2022-04-21 13:03:41 -0500

    feat: switches reactive form component template created

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IPersonTemplateForm } from 'src/app/shared/utils/interfaces/forms.interfaces';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.scss']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
+  person: IPersonTemplateForm = {
+    gender: 'M',
+    canRecibeNotifications: false,
+    isAgreeTyC: false
+  }
 
-  ngOnInit(): void {
+  isFieldValid( fieldName: string ): boolean {
+    // const field = this.peopleForm?.controls[fieldName];
+    // return field?.invalid && 
+    //        field?.touched;
+    return true;
   }
 
 }
